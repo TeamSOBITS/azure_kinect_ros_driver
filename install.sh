c8ed6ad4d34543caff15bc76889766a03a890925
MrKeith99 <kvalentincardenas@hotmail.com>
2023-11-23 13:45:35 +0900

    update sh file with automatic eula confirmation

--- install.sh
+++ install.sh
@@ -6,25 +6,35 @@ echo "╔══╣ Install: k4a-tools for Azure Kinect (STARTING) ╠══╗"
 
 # Install dependencies
 sudo apt-get update
-sudo apt install -y \
+sudo apt-get install -y \
     software-properties-common \
     curl \
-    dialog
+    wget \
+    dialog \
+    libsoundio1
 
-# Add Microsoft’s official GPG key (Use 18.04 for 20.04)
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/prod
+# Download Azure Kinect related packages
+curl -O https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb
+curl -O https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb
+curl -O https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4abt1.1/libk4abt1.1_1.1.2_amd64.deb
+curl -O https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4abt1.1-dev/libk4abt1.1-dev_1.1.2_amd64.deb
+curl -O https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/k/k4a-tools/k4a-tools_1.4.1_amd64.deb
 
-# Set up the stable repository
-curl -sSL https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
+# Install k4a-related packages
+echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
+echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections
+sudo dpkg -i libk4a1.4_1.4.1_amd64.deb
+sudo dpkg -i libk4a1.4-dev_1.4.1_amd64.deb
 
-# Install the latest stable version of k4a
-sudo apt-get update
+echo 'libk4abt1.1 libk4abt1.1/accepted-eula-hash string 03a13b63730639eeb6626d24fd45cf25131ee8e8e0df3f1b63f552269b176e38' | sudo debconf-set-selections
+echo 'libk4abt1.1 libk4abt1.1/accept-eula boolean true' | sudo debconf-set-selections
+sudo dpkg -i libk4abt1.1_1.1.2_amd64.deb
+sudo dpkg -i libk4abt1.1-dev_1.1.2_amd64.deb
 
-sudo apt-get install -y \
-    libk4a1.3-dev \
-    libk4abt1.0-dev \
-    k4a-tools=1.3.0
+sudo dpkg -i k4a-tools_1.4.1_amd64.deb
+
+# Remove deb files
+sudo rm *.deb
 
 # Add rules file
 wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules
@@ -32,6 +42,8 @@ sudo mv 99-k4a.rules /etc/udev/rules.d/
 
 # Install ROS packages for rtabmap demo
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-rtabmap-ros
+    ros-${ROS_DISTRO}-rtabmap-ros \
+    ros-${ROS_DISTRO}-camera-info-manager
+
 
 echo "╚══╣ Install: k4a-tools for Azure Kinect (FINISHED) ╠══╝"
